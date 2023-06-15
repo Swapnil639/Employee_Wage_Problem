67524714e55bd0dd101b6716fdf833e3451fadac
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-15 22:13:25 +0530

    Added :Calculate Employee part time wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,20 +4,27 @@ import java.util.Random;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        int isFullTime=1;
+        int isFullTime = 1;
+        int isPartTime = 2;
         Random random = new Random();
-        int empCheck = random.nextInt(2);
-        int wagePerHour=20;
+        int empCheck = random.nextInt(3);
+        int wagePerHour = 20;
         int empHrs;
         int salary;
 
         if (empCheck == isFullTime) {
             System.out.println("Employee Full Time wage");
-            empHrs=8;
-            salary=(wagePerHour * empHrs);
-            System.out.println(salary);
+            empHrs = 8;
+            salary = (wagePerHour * empHrs);
+            System.out.println("Salary: " + salary);
+        } else if (empCheck == isPartTime) {
+            System.out.println("Employee Part Time wage");
+            empHrs = 4;
+            salary = (wagePerHour * empHrs);
+            System.out.println("Salary: " + salary);
         } else {
             System.out.println("Employee is Absent");
         }
+
     }
 }
