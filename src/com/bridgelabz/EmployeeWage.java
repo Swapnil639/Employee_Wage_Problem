978c5511d9b8f9d48c764b95056341b67188dc21
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-15 21:55:57 +0530

    Added :Check Employee present or not

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee wage computation");
+        Random random = new Random();
+        int empCheck = random.nextInt(2);
+
+        if (empCheck == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
