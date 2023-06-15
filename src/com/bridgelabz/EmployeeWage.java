86174c2dd8debf7a15bc92d0494a2a44ecc052c7
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-16 00:05:23 +0530

    Added :[200~Calculate Wages till

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,13 +5,18 @@ import java.util.Random;
 public class EmployeeWage {
     public final static int Is_Full_Time = 1;
     public final static int Is_Part_Time = 2;
-    static int Wage_Per_Hour = 20;
+    static int WAGE_PER_HOUR = 20;
+    static int NUM_OF_WORKING_HRS = 100;
+    static int NUM_OF_WORKING_DAYS = 20;
 
     public static void main(String[] args) {
-        int empHrs=0;
-        int salary=0;
-        int totalSalary=0;
-        for (int day = 1; day <= 20; day++) {
+        int empHrs = 0;
+        int salary = 0;
+        int totalSalary = 0;
+        int totalEmpHr = 0;
+        int day = 0;
+        while (totalEmpHr < NUM_OF_WORKING_HRS && day < NUM_OF_WORKING_DAYS) {
+            day++;
             Random random = new Random();
             int empCheck = random.nextInt(3);
             switch (empCheck) {
@@ -26,11 +31,13 @@ public class EmployeeWage {
                 default:
                     System.out.println("Employee is Absent");
             }
-            salary = Wage_Per_Hour * empHrs;
-            System.out.println("Day:"+day+"  "+"Salary:"+salary);
-            totalSalary=totalSalary+salary;
+            salary = WAGE_PER_HOUR * empHrs;
+            System.out.println("Day:" + day + "  " + "Salary:" + salary);
+            totalSalary = totalSalary + salary;
+            totalEmpHr = totalEmpHr + empHrs;
         }
-        System.out.println("Total salary: "+totalSalary);
+        System.out.println("Total Employee Hrs:" + totalEmpHr + " " + " Days:" + day);
+        System.out.println("Total salary: " + totalSalary);
 
     }
 }
