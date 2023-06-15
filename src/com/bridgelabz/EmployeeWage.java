1a9783696d2a99305ba851b6f4bd6b46dfa0b62c
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-15 22:05:09 +0530

    Added :Calculate Employee daily wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,11 +4,18 @@ import java.util.Random;
 
 public class EmployeeWage {
     public static void main(String[] args) {
+        int isFullTime=1;
         Random random = new Random();
         int empCheck = random.nextInt(2);
+        int wagePerHour=20;
+        int empHrs;
+        int salary;
 
-        if (empCheck == 1) {
-            System.out.println("Employee is Present");
+        if (empCheck == isFullTime) {
+            System.out.println("Employee Full Time wage");
+            empHrs=8;
+            salary=(wagePerHour * empHrs);
+            System.out.println(salary);
         } else {
             System.out.println("Employee is Absent");
         }
