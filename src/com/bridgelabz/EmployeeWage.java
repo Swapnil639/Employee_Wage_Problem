54e3302b822edf6c7fe5d5bbadd0db78d570d361
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-15 22:48:52 +0530

    Added :Calculate employee wage using case

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,27 +3,31 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class EmployeeWage {
+    public final static int Is_Full_Time = 1;
+    public final static int Is_Part_Time = 2;
+    static int Wage_Per_Hour = 20;
+
     public static void main(String[] args) {
-        int isFullTime = 1;
-        int isPartTime = 2;
         Random random = new Random();
         int empCheck = random.nextInt(3);
-        int wagePerHour = 20;
         int empHrs;
         int salary;
 
-        if (empCheck == isFullTime) {
-            System.out.println("Employee Full Time wage");
-            empHrs = 8;
-            salary = (wagePerHour * empHrs);
-            System.out.println("Salary: " + salary);
-        } else if (empCheck == isPartTime) {
-            System.out.println("Employee Part Time wage");
-            empHrs = 4;
-            salary = (wagePerHour * empHrs);
-            System.out.println("Salary: " + salary);
-        } else {
-            System.out.println("Employee is Absent");
+        switch (empCheck) {
+            case Is_Full_Time:
+                System.out.println("Employee Full Time wage");
+                empHrs = 8;
+                salary = (Wage_Per_Hour * empHrs);
+                System.out.println("Salary: " + salary);
+                break;
+            case Is_Part_Time:
+                System.out.println("Employee Part Time wage");
+                empHrs = 4;
+                salary = (Wage_Per_Hour * empHrs);
+                System.out.println("Salary: " + salary);
+                break;
+            default:
+                System.out.println("Employee is Absent");
         }
 
     }
