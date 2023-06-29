e25eacdc85cab917485c0781edd330a1bd778214
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-29 12:20:54 +0530

    Added :compute employee wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -9,14 +9,14 @@ public class EmployeeWage {
     static int NUM_OF_WORKING_HRS = 100;
     static int NUM_OF_WORKING_DAYS = 20;
 
-    public static void main(String[] args) {
+    public static void computeWage() {
         int empHrs = 0;
-        int salary = 0;
+        int salary;
         int totalSalary = 0;
         int totalEmpHr = 0;
-        int day = 0;
-        while (totalEmpHr < NUM_OF_WORKING_HRS && day < NUM_OF_WORKING_DAYS) {
-            day++;
+        int totalWorkingDay = 0;
+        while (totalEmpHr < NUM_OF_WORKING_HRS && totalWorkingDay < NUM_OF_WORKING_DAYS) {
+            totalWorkingDay++;
             Random random = new Random();
             int empCheck = random.nextInt(3);
             switch (empCheck) {
@@ -32,12 +32,15 @@ public class EmployeeWage {
                     System.out.println("Employee is Absent");
             }
             salary = WAGE_PER_HOUR * empHrs;
-            System.out.println("Day:" + day + "  " + "Salary:" + salary);
+            System.out.println("Day:" + totalWorkingDay + "  " + "Salary:" + salary);
             totalSalary = totalSalary + salary;
             totalEmpHr = totalEmpHr + empHrs;
         }
-        System.out.println("Total Employee Hrs:" + totalEmpHr + " " + "Total Working Days:" + day);
-        System.out.println("Total salary: " + totalSalary);
+        System.out.println("Total Employee Hrs:" + totalEmpHr + " " + "Total Working Days:" + totalWorkingDay);
+        System.out.println("Total salary: " + totalSalary);                     
+    }
 
+    public static void main(String[] args) {
+        computeWage();
     }
 }
